be04a7ad7c2ebe370862e883bf50015c6848a763
Mohamed Idris Maricar <idrismaric@gmail.com>
2023-06-26 04:34:33 +0400

    Update dxb-price-predictor.py

--- dxb-price-predictor.py
+++ dxb-price-predictor.py
@@ -1,9 +1,8 @@
-#import subprocess
-#subprocess.check_call(['pip', 'install', 'xgboost'])
+
 
 
 import streamlit as st
-#import xgboost as xgb 
+
 import pandas as pd
 
 #model = xgb.Booster()
@@ -14,8 +13,28 @@ training_data = pd.read_csv('ml_inputBus.csv')
 
 st.write(""" 
 # Price prediction for the Property in Business Bay
+#import subprocess
+#subprocess.check_call(['pip', 'install', 'xgboost'])
+#import xgboost as xgb 
+#st.dataframe(input_data)
+
+dict = input_data.to_dict(orient='list')
+new_dict = {key: value[0] for key, value in dict.items()}
+mod_dict = {f'{key}_{value}' if isinstance(value, str) else key: (value if isinstance(value, (int, float)) else 1) for key, value in new_dict.items()}
+training_columns = training_data.columns
+new_data_df = pd.DataFrame(columns=training_columns)
+new_data_df = new_data_df.append(mod_dict, ignore_index = True)
+new_data_df = new_data_df.fillna(0)
 
 
+ml_input = new_data_df.iloc[:,1:]
+ml_input = ml_input.values
+
+ml_input =  xgb.DMatrix(ml_input)
+
+prediction = model.predict(ml_input)
+
+st.write("The Predicted value of the Property:", prediction)
 """)
 
 styled_table = (
@@ -28,7 +47,7 @@ styled_table = (
 st.markdown(styled_table, unsafe_allow_html=True)
 
 
-#st.dataframe(input_data)
+
 
 dict = input_data.to_dict(orient='list')
 new_dict = {key: value[0] for key, value in dict.items()}
